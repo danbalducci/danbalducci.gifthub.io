e010d4086da8498d65057c7d240b89c13bf59baa
danbalducci <32689920+danbalducci@users.noreply.github.com>
2018-04-05 12:12:26 -0700

    changesjs

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 ### I am an artist. This is my portfolio site.
 
 This is a *README* file for my repository. It is written using **Markdown**.
+
+
+jig
--- sketch.js
+++ sketch.js
@@ -1,22 +1,33 @@
-let img;
-let x = 0;
+let students = []
 
-function preload() {
-  img = loadImage('assets/laDefense.jpg');
+let students = {
+  firstName: "Jackie",
+  lastName: "Bibi",
+  year: "Sophmore"
+
 }
 
 function setup() {
-  createCanvas(500, 500);
-  background(200);
-  // If you want to scale the image to 100 x 100 pixels
-  // image(img, 0, 0, 100, 100);
 
-  // If you want to scale image by 50%
-  // image(img, 10, 10, img.width/2, img.height/2);
+console.log(student.firstName)
+
+students = [0] + "Jackie";
+students = [1] + "Monica";
+
+students.push("hAndy");
+students.push("sAndy");
+students.push("vAndy");
+students.push("bAndy");
+students.push("nAndy");
+students.push("mAndy");
+students.push("hAndy");
+students.push("jAndy");
+students.push("kAndy");
+students.push("lAndy");
+
+for (i = 0; i < students.length; 1++){
+c`${students[i]} is a great student`);
 }
 
-function draw() {
-  image(clouds, x, 10, clouds.width/2, clouds.height/2)
-x++
-if (x < 0) {
-  x = height;
+
+}
